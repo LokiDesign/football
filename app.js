b832f081216026b33c887c2909965596d5c90b23
Andrew Dickerson <olah89@gmail.com>
2013-08-11 00:44:48 -0700

    Ajax call request cross server on behalf of page

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -34,6 +33,29 @@ app.all('/', routes.index);
 app.all('/scores', scores.index);
 app.all('/leagues', leagues.index);
 app.all('/rightarm', rightarm.index);
+app.get('/getLeague/28655', function(req, res){
+    var http = require('http');
+    var options = {
+        host: 'football32.myfantasyleague.com',
+        path: '/2013/export?TYPE=league&L=28655&W=&JSON=1'
+    };
+    var franchises;
+    http.get(options, function(res) {
+        var data = '';
+        res.on('data', function (chunk){
+            data += chunk;
+        });
+        res.on('end',function(){
+            var obj = JSON.parse(data);
+            franchises = obj.league.franchises.franchise;
+            console.log('DATA: ' + JSON.stringify(franchises[0]))
+        })
+    }).on('close', function(){
+        res.send(franchises);
+    }).on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+})
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/rightarm/index.js
+++ routes/rightarm/index.js
@@ -1,5 +1,7 @@
 exports.index = function(req, res){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    
+
     res.render('rightarm', { title: 'RightArm' });
 };
