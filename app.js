7132b34010cb5cc1d486b490a51bc8871e9ed50d
Andrew Dickerson <olah89@gmail.com>
2013-08-11 01:56:13 -0700

    Added nfl schedule page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express = require('express'),
  leagues = require('./routes/leagues'),
  rightarm = require('./routes/rightarm'),
  franchises = require('./routes/rightarm/franchises'),
+ schedule = require('./routes/schedule'),
  http = require('http'),
  path = require('path');
 
@@ -34,29 +35,40 @@ app.all('/scores', scores.index);
 app.all('/leagues', leagues.index);
 app.all('/rightarm', rightarm.index);
 app.all('/rightarm/franchises', franchises.index);
-app.get('/getLeague/28655', function(req, res){
-    var http = require('http');
-    var options = {
-        host: 'football32.myfantasyleague.com',
-        path: '/2013/export?TYPE=league&L=28655&W=&JSON=1'
-    };
-    var franchises;
-    http.get(options, function(res) {
-        var data = '';
-        res.on('data', function (chunk){
-            data += chunk;
-        });
-        res.on('end',function(){
-            var obj = JSON.parse(data);
-            franchises = obj.league.franchises.franchise;
-            console.log('DATA: ' + JSON.stringify(franchises[0]))
-        })
-    }).on('close', function(){
-        res.send(franchises);
-    }).on('error', function(e) {
-        console.log('ERROR: ' + e.message);
-    });
-})
+app.all('/schedule', schedule.index);
+
+app.get('/getLeague/28655', jsonData({
+    host: 'football32.myfantasyleague.com',
+    path: '/2013/export?TYPE=league&L=28655&W=&JSON=1'
+}));
+
+app.get('/getSchedule', jsonData({
+    host: 'football.myfantasyleague.com',
+    path: '/2013/export?TYPE=nflSchedule&W=1&JSON=1'
+}));
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function jsonData(options){
+    return function(req, res){
+        var http = require('http');
+        var dat;
+        http.get(options, function(res) {
+            var data = '';
+            res.on('data', function (chunk){
+                data += chunk;
+            });
+            res.on('end',function(){
+                var obj = JSON.parse(data);
+                dat = obj;
+            })
+        }).on('close', function(){
+            res.send(dat);
+        }).on('error', function(e) {
+            console.log('ERROR: ' + e.message);
+        });
+    }
+}
